94b2da75b69d79da7082f19912c8157969ec91f1
dusty-jrr <2822270901@qq.com>
2021-03-08 20:04:50 +0800

    完成了权限功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -23,7 +23,11 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -48,5 +52,9 @@ Vue.use(Button)
   .use(Tooltip)
   .use(Pagination)
   .use(Dialog)
+  .use(Tag)
+  .use(Tree)
+  .use(Select)
+  .use(Option)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
